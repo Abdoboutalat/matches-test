292517b0c3dd71402f9174689fdff9f61f7f8f6e
Abdelfattah Boutalat <aboutalat@squad.fr>
2020-02-05 17:27:00 +0100

    add material design and  first part of the game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'abdel-matches-game';
+export class AppComponent implements OnInit{
+    matchesNumber: number;
+
+  ngOnInit(): void {
+      this.matchesNumber =  this.initMatchesNumber();
+  }
+  pickMatches(pickedMatchesNumber: number) {
+    this.matchesNumber -= pickedMatchesNumber;
+  }
+  initMatchesNumber(){
+    return this.matchesNumber =  Math.floor(Math.random() * (15 - 5) + 5);
+  }
 }
